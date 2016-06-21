8348af074fb9de93449fefed9aae24dea944380e
Lê Hoàn Hảo <lehoanhao.94@gmail.com>
2016-06-21 17:54:29 +0700

    abcaa

---
+++ TGMCO/TGMCO/Controllers/PAGECONTROLLER/NewsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TGMCO.Controllers.PAGECONTROLLER
+{
+    public class NewsController : Controller
+    {
+        public ActionResult News()
+        {
+            try
+            {
+                Session["SUPPLIER"] = "DEFAULT";
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Http404", "Error"); // 404
+            }
+        }
+
+        public ActionResult NewsPromotion()
+        {
+            try
+            {
+                Session["SUPPLIER"] = "DEFAULT";
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Http404", "Error"); // 404
+            }
+        }
+    }
+}
